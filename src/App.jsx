42e40486d99e125967939e9bc0f80e9ce4538a7b
Pedro Benegas <pedrophbs8199@gmail.com>
2025-01-26 15:33:20 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>MinibBlog</h1>
+    </>
+  );
+}
+
+export default App;
