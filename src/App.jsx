090e566182a2ec92b761c3c5ee3e78e3b328d34a
Pedro Benegas <pedrophbs8199@gmail.com>
2025-01-26 19:55:45 -0300

    add: initital pages, components,  hooks and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
+// CSS
 import "./App.css";
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0);
+// Pages
+import Home from "./pages/Home/Home";
+import About from "./pages/about/About";
 
+// Components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+
+function App() {
   return (
     <>
-      <h1>MinibBlog</h1>
+      <BrowserRouter>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
